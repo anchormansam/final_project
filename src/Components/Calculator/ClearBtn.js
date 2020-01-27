68102b86eb2794533b997596fad4f0e2252ac94e
Sam Wheeling <anchormansam@gmail.com>
2020-01-27 18:26:29 -0500

    -finishimg up logic of the calculator -still need to work on the clear button click handle function -still need to get the math on using an operator and a decimal

--- src/Components/Calculator/ClearBtn.js
+++ src/Components/Calculator/ClearBtn.js
@@ -2,14 +2,17 @@ import React from "react";
 import "./ClearBtn.css";
 
 class ClearBtn extends React.Component {
- 
   render() {
-    return (
-     <div className="clear-btn">
-         {this.props.children}
-     </div>
-    );
+    return( 
+      <div 
+        className="clear-btn"
+        onClick={() => this.props.handleClear()}
+      >
+        {this.props.children}
+      </div>
+    )
   }
 }
 
 export default ClearBtn;
+
